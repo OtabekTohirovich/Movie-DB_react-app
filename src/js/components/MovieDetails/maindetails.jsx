2c1ec8b4b53a5b25b134c5e1a77137a165b3daf8
OtabekTohirovich <tohirovo630@gmail.com>
2023-02-15 20:58:56 +0500

    added same changes

--- src/js/components/MovieDetails/maindetails.jsx
+++ src/js/components/MovieDetails/maindetails.jsx
@@ -1,4 +1,19 @@
-import { Box, CardMedia, Container, Stack, Typography } from "@mui/material";
+import {
+  Bookmark,
+  BookmarkBorder,
+  Favorite,
+  FavoriteBorder,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Checkbox,
+  Container,
+  Rating,
+  Stack,
+  Typography,
+} from "@mui/material";
 import config from "../../../configs";
 import { collors } from "../../../constants/color";
 import Progress from "../main/searclular-progress";
@@ -16,24 +31,27 @@ const Maindetails = ({ vediodetail }) => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         boxShadow: "inset 3000px 3000px 8000px #403636d9",
+        width: "100%",
+        height: '90vh'
       }}
     >
       <Container>
-        <Stack direction={"row"}>
+        <Stack direction={"row"} sx={{margin: "100px 0"}}>
           <Box>
             <CardMedia
               image={config.baseImgURL + vediodetail.poster_path}
               alt={vediodetail.title}
               sx={{
                 width: "350px",
-                height: "450px",
+                height: "550px",
                 objectFit: "cover",
                 borderRadius: "15px",
-                margin: "30px 0",
+                display: "flex",
+                
               }}
             />
           </Box>
-          <Box sx={{ padding: "45px 30px" }}>
+          <Box sx={{ margin: "100px 50px" , color: collors.white }}>
             <Typography
               variant="p"
               fontWeight={"700"}
@@ -41,17 +59,71 @@ const Maindetails = ({ vediodetail }) => {
             >
               {vediodetail.title}
             </Typography>
-            <div
-              style={{
-                background: collors.mainBlue,
-                padding: "6px",
-                borderRadius: "30px",
-                width: "48px",
-                transform: "scale(1.4)"
-              }}
+            <Stack
+              direction={"row"}
+              sx={{ display: "flex", alignItems: "center", gap: "20px" }}
             >
-              <Progress vote_average={vediodetail.vote_average} />
-            </div>
+              <div
+                style={{
+                  background: collors.mainBlue,
+                  padding: "6px",
+                  borderRadius: "30px",
+                  width: "48px",
+                  height: "48px",
+                  transform: "scale(1.3)",
+                }}
+              >
+                <Progress vote_average={vediodetail.vote_average} />
+              </div>
+
+              <Typography
+                width={"22px"}
+                sx={{
+                  margin: "10px",
+                  fontWeight: "bold",
+                  color: collors.white,
+                }}
+              >
+                User Score
+              </Typography>
+              <div
+                style={{
+                  background: collors.mainBlue,
+                  borderRadius: "50px",
+                  padding: "10px",
+                }}
+              >
+                <Rating name="half-rating" defaultValue={0} precision={0.5} />
+              </div>
+
+              <div
+                style={{
+                  background: collors.mainBlue,
+                  borderRadius: "50px",
+                  padding: "3px",
+                }}
+              >
+                <Checkbox
+                  icon={<FavoriteBorder sx={{ color: collors.white }} />}
+                  checkedIcon={<Favorite sx={{ color: "#ef47b6" }} />}
+                />
+              </div>
+              <div
+                style={{
+                  background: collors.mainBlue,
+                  borderRadius: "50px",
+                  padding: "3px",
+                }}
+              >
+                <Checkbox
+                  icon={<BookmarkBorder sx={{ color: collors.white }} />}
+                  checkedIcon={<Bookmark sx={{ color: "#cf3131" }} />}
+                />
+              </div>
+            </Stack>
+            <Typography variant="h5" marginTop={'30px'} fontWeight={"bold"}>{vediodetail.release_date}</Typography>
+            <Typography marginTop={'10px'} fontWeight={"bold"}>{vediodetail.status}</Typography>
+            <Button  variant="outlined" sx={{ color: "#fff", borderColor: "#fff" , marginTop: '40px'}}>Show vedio</Button>
           </Box>
         </Stack>
       </Container>
